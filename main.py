9c14b103f69605b9e7b40b06ac79dce847f66489
cosmic-elevator <rose_suzy@naver.com>
2023-12-03 10:59:49 +0900

    'asdf'

--- main.py
+++ main.py
@@ -28,15 +28,48 @@ import pygame
 pygame.init()
 pygame.mixer.init()
 
-playbutton = pygame.image.load('playbutton.png')
-pygame.mixer.music.load('test.wav')
-MusicChannel = pygame.mixer.Channel(1)
+
+class Note(pygame.sprite.Sprite):
+    def __init__(self, linenum, exact_hit_time):
+        super(Note, self).__init__()
+        self.linenum = linenum
+        self.exact_hit_time = exact_hit_time
+        self.image = pygame.image.load('purple_note.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = 440
+        self.rect.y = 0
+
+    def drop(self):
+        self.rect.y += 10
+        SCREEN.blit(self.image, (self.rect.x, self.rect.y))
+        if self.rect.y >= 570:
+            self.kill()
+    
+
 
 WIDTH = 1280
 HEIGHT = 720
+FPS = 60
+SPEED = 10  # 1프레임당 내려오는 노트의 픽셀
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 pygame.display.set_caption('rhythm game test')
+
+smallfont = pygame.font.Font(None, 40)
+mediumfont = pygame.font.Font(None, 50)
+bigfont = pygame.font.Font(None, 80)
+
+playbutton = pygame.image.load('playbutton.png')
+pink_keybeam = pygame.image.load('pink_keybeam.png')
+judgeline = pygame.Rect(WIDTH/2-100, HEIGHT/2, 200, 10)
+pygame.mixer.music.load('test.wav')
+MusicChannel = pygame.mixer.Channel(1)
+
+clock = pygame.time.Clock()
+music_start_time = 0
+music_playtime = 0
+
+notequeue = [Note(1, 1), Note(1, 2), Note(1, 3), Note(1, 4), Note(1, 5), Note(1, 6), Note(1, 7), Note(1, 8)]
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -50,9 +83,11 @@ def music_play():
     if not pygame.mixer.get_busy():
         MusicChannel.play(pygame.mixer.Sound('test.wav'))
 
-SCREEN.fill(BLACK)
-SCREEN.blit(playbutton, (0, 0))
-pygame.display.flip()
+
+def timing(note):
+    key_press_time = pygame.time.get_ticks()
+    if abs(key_press_time - note.exact_hit_time) >= 0 and abs(key_press_time - note.exact_hit_time) <= 400:
+       SCREEN.blit(mediumfont.render("Perfect", True, YELLOW), (540, 400))
 
 is_running = True
 while is_running:
@@ -61,6 +96,28 @@ while is_running:
             is_running = False
         if event.type == pygame.KEYDOWN and event.key == pygame.K_UP:
             music_play()
+            music_start_time = pygame.time.get_ticks()
+
+    SCREEN.fill(BLACK)
+    pygame.draw.rect(SCREEN, GREEN, [WIDTH/2-200, HEIGHT/2+140, 400, 10])   # 540, 500, 200, 10
+
+    for note in notequeue:
+        if music_playtime/1000 >= note.exact_hit_time - (500 / (FPS * SPEED)):
+            note.drop()
+
+        
+    if pygame.key.get_pressed()[pygame.K_d]:
+        SCREEN.blit(pink_keybeam, (440, 0))
+        timing(notequeue[0])
+        #SCREEN.blit(mediumfont.render("Perfect", True, YELLOW), (560, 400))
+
+    if music_start_time > 0 and music_playtime <= 8000:
+        music_playtime = pygame.time.get_ticks() - music_start_time
+    SCREEN.blit(bigfont.render(str(music_playtime/1000), True, WHITE), (20, 20))
+        
+
+    pygame.display.flip()
+    clock.tick(FPS)
 
 
 pygame.quit()
